40aebcb1340003ca9aeaa67df942f7b8afc35880
arpan <arpanchand3@gmail.com>
2023-08-13 19:37:02 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -4,6 +4,6 @@ export const connectDB=()=>{
     mongoose.connect(process.env.MONGO_URL, {
         dbName:"API",
     })
-    .then(()=>console.log("Database conected"))
+    .then((c)=>console.log(`Database conected ${c.connection.host}`))
     .catch((e) => console.log(e));
 }
